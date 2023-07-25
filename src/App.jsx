3cd4685b52417b429dbca6482d30610d60d4c900
Carlos <charlyqv98@gmail.com>
2023-07-25 09:21:52 -0600

    Tarea - decrement e incrementBy

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import { increment } from './store/slices/counter';
+import { decrement, increment, incrementBy } from './store/slices/counter';
+import { useState } from 'react';
 
 function App() {
 
   const { counter } = useSelector( state => state.counter );
   const dispatch = useDispatch();
+  // const [incrementtwo, setIncrementtwo] = useState('2')
 
   return (
     <>
@@ -20,10 +22,16 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>count is { counter }</h1>
       <div className="card">
         <button onClick={ () => dispatch( increment() )}>
-          count is { counter }
+          Increment
+        </button>
+        <button onClick={ () => dispatch( decrement() )}>
+          Decrement
+        </button>
+        <button onClick={ () => dispatch( incrementBy(2) )}>
+          Increment by 2
         </button>
       </div>
     </>
